9f8c0e13b5e68cf0c0cae24f875ff06b5c8bd026
Firuz43 <firuz1199@mail.ru>
2022-04-16 14:17:36 +0400

    add Navbar.js

--- main/frontend/src/components/Header.js
+++ main/frontend/src/components/Header.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <h3>Logo</h3>
+      <nav>
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>Login</a></li>
+          <li><a>Register</a></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
