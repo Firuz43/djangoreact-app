da83fc0e3bec63fa08c312668ad9b21dd7629531
Firuz43 <firuz1199@mail.ru>
2022-04-21 03:27:52 +0400

    style

--- main/frontend/src/components/Header.js
+++ main/frontend/src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className="nav-links">
       <h3>Logo</h3>
       <nav>
-        <ul className="nav-links">
+        <ul className="anchor">
           <li><a>Home</a></li>
           <li><a>Login</a></li>
           <li><a>Register</a></li>
